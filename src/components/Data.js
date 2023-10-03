6369d0fb2412cbcdbccd0ef412f8610d37425bba
rsotomay <138070004+rsotomay@users.noreply.github.com>
2023-10-02 22:30:01 -0500

    Adds to Data component

--- src/components/Data.js
+++ src/components/Data.js
@@ -8,15 +8,15 @@ const Data = ({ cost, totalSupply }) => {
         {ethers.formatUnits(cost, "ether")} ETH
       </p>
       <p className="text-end mx-5" style={{ color: "silver" }}>
-        <strong className="mx-1">Total Supply:</strong>
+        <strong className="mx-1">Total Minted:</strong>
         {totalSupply.toString()} NFTs
       </p>
       <p
         className="text-center mx-5"
         style={{ fontSize: 20, color: "silver", padding: "2rem" }}
       >
-        DecentralArt-AI invites you to be the mastermind behind your digital
-        masterpieces. Simply provide a prompt, and our cutting-edge AI
+        DecentralArt-AI invites you to create your next digital masterpieces
+        with the help of AI. Simply provide a prompt, and our cutting-edge AI
         algorithms will spring into action, crafting stunning and unique
         artworks tailored to your vision. Whether you seek abstract beauty,
         vibrant landscapes, or futuristic designs, DecentralArt-AI transforms
